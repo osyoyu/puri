9deba16e215f41f859f73744024354f0045e76c5
osyoyu <osyoyu@osyoyu.com>
2015-07-15 14:33:58 +0900

    Slow slow FFT

--- Gemfile
+++ Gemfile
@@ -1,3 +1,6 @@
 source 'https://rubygems.org'
 
 gem 'wav-file'
+gem 'ruby-fftw3'
+
+gem 'stackprof'
---
+++ puri.rb
@@ -0,0 +1,22 @@
+require 'wav-file'
+require 'stackprof'
+
+require_relative 'fft.rb'
+
+f = open("./sounds/make_it-15.wav")
+format     = WavFile::readFormat(f)
+data_chunk = WavFile::readDataChunk(f)
+f.close
+
+data = data_chunk.data.unpack("c*")
+
+# pad data_chunk with zeros so data_chunk.length
+n = 2
+n = n * 2 until n >= data.length
+data += Array.new(n - data.length, 0)
+
+StackProf.run(mode: :cpu, out: 'stackprof-cpu.dump') do
+  ffter = Puri::FFT.new(data.size, 44100)
+  fft = ffter.fft(data)
+end
+#  puts "#{i} #{a}"
